1c06150e897a8ed84af08c6ec2b79ebb86d4de90
Eduardo Bouças <mail@eduardoboucas.com>
2016-09-25 11:41:54 +0100

    Add Connect tool

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,27 @@ var SpeedTracker = (function ($) {
 
       return false;
     })
+
+    $('form[name="connect"]').submit(function () {
+      var $target = $('.js-connect-result');
+      var baseUrl = $(this).attr('action');
+
+      var username = $(this).find('[name="username"]').val();
+      var repository = $(this).find('[name="repository"]').val();
+      var branch = $(this).find('[name="branch"]').val();
+
+      var url = baseUrl + '/' + username + '/' + repository + '/' + branch
+
+      $target.text('Hang on...');
+
+      $.get(url, function (data) {
+        $target.text(data);
+      }).fail(function () {
+        $target.html('Oops, something went wrong. Did you invite <strong>speedtracker-bot</strong> to your repository?');
+      });
+
+      return false;
+    })
   }
 
   $(document).init(function () {
